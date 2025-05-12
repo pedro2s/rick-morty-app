7117fbd56261bb100191a4bc7f8a347427f5c8e9
Pedro Santana <pedromanu27@gmail.com>
2025-05-12 20:57:38 -0300

    feat: :sparkles: Quasar configurado

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,29 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+// Quasar
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins se necesario
+  config: {
+    brand: {
+      primary: '#1976D2',
+      secondary: '26A69A',
+      accent: '#9C27B0',
+      dark: '#1D1D1D',
+      positive: '#21BA45',
+      negative: '#C10015',
+      info: '#31CCEC',
+      warning: '#F2C037'
+    }
+  }
+})
 
 app.mount('#app')
